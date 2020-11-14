66ae848e6a126857d876f1bf8fc20978dbd693e0
Adrian Jagiełło <adrian.jagiello@icloud.com>
2020-11-14 19:20:39 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const firstName = 'Adrian';
+const lastName = "Jagiełło";
+const age = 22;
+
+console.log(firstName);
+console.log(age);
+console.log(`Siema kurwa, nazywam się ${firstName} ${lastName} i mam ${age} lata XD`)
